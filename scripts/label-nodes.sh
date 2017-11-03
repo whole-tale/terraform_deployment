dbb7f7cf86c0109b24058b1731253cf05a644940
Craig Willis <willis8@illinois.edu>
2017-11-03 15:39:57 +0000

    Added core label

--- scripts/label-nodes.sh
+++ scripts/label-nodes.sh
@@ -1,11 +1,11 @@
 # Label the manager node as a mongo replica
-docker node update --label-add mongo.replica=1 $(docker node ls -q -f "role=manager")
+docker node update --label-add mongo.replica=1 --label-add core=1 $(docker node ls -q -f "role=manager")
 
 
 # Label the first two worker nodes as mongo replicas
 i=2
 for node in `docker node ls -q -f "role=worker" | head -2`
 do
-    docker node update --label-add mongo.replica=$i $node
+    docker node update --label-add mongo.replica=$i --label-add core=1 $node
     ((i++))
 done
