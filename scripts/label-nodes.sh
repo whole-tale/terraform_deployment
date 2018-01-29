2b9d73dd761929079b7ad7d40f0fd6d61e834cdf
Craig Willis <willis8@illinois.edu>
2018-01-29 12:03:39 +0000

    Fixed storage node labeling

--- scripts/label-nodes.sh
+++ scripts/label-nodes.sh
@@ -6,7 +6,7 @@ docker node update --label-add mongo.replica=1 --label-add core=1 $(docker node 
 
 
 # Label the storage node
-docker node update --label-add storage=1 --label-add core=1 `docker node ls -q -f "name=$nfs_host"`
+docker node update --label-add storage=1 --label-add core=1 $(docker node ls -q -f "name=$nfs_host")
 
 # Label the first two worker nodes as mongo replicas
 echo "Labeling mongo replicas"
