c5262f23e5284ee3fc9ebc7c72c03688b8455132
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2018-02-21 21:54:19 -0600

    Add all the magic that needs to happen for celery_worker's FUSE mount to work

--- scripts/start-worker.sh
+++ scripts/start-worker.sh
@@ -3,12 +3,13 @@
 domain=$1 
 role=$2
 
+sudo umount /usr/local/lib > /dev/null 2>&1 || true
 docker stop celery_worker >/dev/null 2>&1
 docker rm celery_worker > /dev/null 2>&1 
 
 docker pull wholetale/gwvolman:dev > /dev/null 2>&1
 
-docker run --privileged \
+docker run \
     --name celery_worker \
     --label traefik.enable=false \
     -e GIRDER_API_URL=https://girder.${domain}/api/v1 \
@@ -16,13 +17,25 @@ docker run --privileged \
     -e TRAEFIK_NETWORK=wt_traefik-net \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v /:/host \
+    -v /var/cache/davfs2:/var/cache/davfs2 \
+    -v /run/mount.davfs:/run/mount.davfs \
     --device /dev/fuse \
     --cap-add SYS_ADMIN \
+    --cap-add SYS_PTRACE \
     --network wt_celery \
     -d --entrypoint=/usr/bin/python \
     wholetale/gwvolman:dev \
       -m girder_worker -l info \
       -Q ${role},$(docker info --format "{{.Swarm.NodeID}}") \
       --hostname=$(docker info --format "{{.Swarm.NodeID}}")
+
+sudo mount --bind \
+  $(docker inspect --format={{.GraphDriver.Data.MergedDir}} celery_worker)/usr/local/lib \
+  /usr/local/lib
+
+docker exec -ti celery_worker chown davfs2:davfs2 /host/run/mount.davfs
+docker exec -ti celery_worker chown davfs2:davfs2 /host/var/cache/davfs2
+[[ -z $(getent group 100) ]] && sudo groupadd -g 100 wtgroup
+[[ -z $(getent passwd 1000) ]] && sudo useradd -g 100 -u 1000 wtuser
 
 sleep 10
